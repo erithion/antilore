292f82f032ed8e39c41d24c68711ae402e6d68c6
Artyom Bondartsov <erithion@gmail.com>
2018-08-09 03:55:45 +0300

    matlab research environment - added transfer fn, ccde and filter test on sinusoids

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # antilore
-Experimental audio-filter
+Experimental VideoLAN audio-filter
 
 
-## Matlab environment
+## Matlab preliminary calc results
 
+### A simple test signal (two frequencies)
 ![Filter in Matlab](https://github.com/erithion/antilore/raw/master/doc/matlab_cut_filter.png  "Filter in Matlab")
+
+### A simple test signal (modulation within)
+![Filter in Matlab](https://github.com/erithion/antilore/raw/master/doc/matlab_cut_filter_modulated.png  "Filter in Matlab")
--- matlab/testCut.m
+++ matlab/testCut.m
@@ -9,18 +9,20 @@ figure('Name','Filter test on sinusoids');
 % оригинал
 t = linspace(0, 1000 - f, 1000);
 orig = 2 * sin(w * t) + 4 * sin(2 * w * t);
+%orig = 2 * sin(w * t) + (cos(t * w/6) .* (4 * sin(2 * w * t)));
 subplot(4, 1, 1);
 plot(t, orig);
 grid on;
-title('Original signal');
+title('Original (modulated) signal');
 ylabel('2 sin \pi/10 + 4 sin \pi/5');
+%ylabel('2 sin \pi/10 + cos \pi/60 * 4 sin \pi/5');
 
 % FFT
 n = length(orig); 
 ORIG = fftshift(fft(orig, n));
 subplot(4, 1, 2);
 tt = - pi : 2 * pi / n : pi - 2 * pi / n;
-plot( tt, abs(ORIG));
+plot(tt, abs(ORIG));
 grid on;
 ylabel('Magnitude');
 title('FFT');
